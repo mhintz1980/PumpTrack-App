0030750cc9e8eb4f1ddb93795eb9e4f445e14109
Mark Hintz <mhintz1980@gmail.com>
2025-06-05 17:40:06 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/components/layout/RootLayoutClientWrapper.tsx
+++ src/components/layout/RootLayoutClientWrapper.tsx
@@ -21,7 +21,7 @@ import { LayoutDashboard, BarChart2, CalendarDays, BrainCircuit } from 'lucide-r
 
 // Inner component to use the useSidebar hook correctly as it needs to be a child of SidebarProvider
 function SidebarNavigationItems() {
-  const pathname = usePathname(); // usePathname can be called here again or passed as prop
+  const pathname = usePathname();
   const { open, isMobile } = useSidebar();
 
   const sidebarNavItems = [
@@ -40,7 +40,6 @@ function SidebarNavigationItems() {
             <SidebarMenuButton tooltip={item.label} isActive={pathname === item.href} asChild>
               <Link href={item.href}>
                 <Icon />
-                {/* Conditionally render the label text based on sidebar state */}
                 {(isMobile || open) && <span>{item.label}</span>}
               </Link>
             </SidebarMenuButton>
@@ -57,19 +56,21 @@ export function RootLayoutClientWrapper({ children }: { children: ReactNode }) {
     <SidebarProvider defaultPinned={false}>
       <Sidebar side="left" variant="sidebar">
         <SidebarHeader>
-          <div className="flex items-center gap-2 p-2">
-            <svg width="24" height="24" viewBox="0 0 100 100" className="text-sidebar-primary">
-              <path fill="currentColor" d="M87.7,43.1a6.4,6.4,0,0,0-11.3,0L64,58.2V26.3a6.4,6.4,0,0,0-12.8,0V58.2L38.7,43.1a6.4,6.4,0,0,0-11.3,0L12.3,58.2a6.4,6.4,0,0,0,0,11.3l19.1,19.1a6.4,6.4,0,0,0,11.3,0L55.5,75.8a6.4,6.4,0,0,0,0-11.3L40.4,51.7l9.6-9.6V73.7a6.4,6.4,0,0,0,12.8,0V42.1l9.6,9.6L57.2,64.5a6.4,6.4,0,0,0,0,11.3l12.8,12.8a6.4,6.4,0,0,0,11.3,0l19.1-19.1a6.4,6.4,0,0,0,0-11.3ZM50,12.5a6.3,6.3,0,1,0,6.3,6.2A6.2,6.2,0,0,0,50,12.5Z"/>
-            </svg>
-            {/* The group-data-[state=collapsed]:hidden on the span correctly uses the 'state' from SidebarContext */}
-            <span className="text-lg font-semibold text-sidebar-foreground group-data-[state=collapsed]:hidden">PumpTrack</span>
+          <div className="flex items-center justify-between gap-2 p-2">
+            <div className="flex items-center gap-2">
+              <svg width="24" height="24" viewBox="0 0 100 100" className="text-sidebar-primary">
+                <path fill="currentColor" d="M87.7,43.1a6.4,6.4,0,0,0-11.3,0L64,58.2V26.3a6.4,6.4,0,0,0-12.8,0V58.2L38.7,43.1a6.4,6.4,0,0,0-11.3,0L12.3,58.2a6.4,6.4,0,0,0,0,11.3l19.1,19.1a6.4,6.4,0,0,0,11.3,0L55.5,75.8a6.4,6.4,0,0,0,0-11.3L40.4,51.7l9.6-9.6V73.7a6.4,6.4,0,0,0,12.8,0V42.1l9.6,9.6L57.2,64.5a6.4,6.4,0,0,0,0,11.3l12.8,12.8a6.4,6.4,0,0,0,11.3,0l19.1-19.1a6.4,6.4,0,0,0,0-11.3ZM50,12.5a6.3,6.3,0,1,0,6.3,6.2A6.2,6.2,0,0,0,50,12.5Z"/>
+              </svg>
+              <span className="text-lg font-semibold text-sidebar-foreground group-data-[state=collapsed]:hidden">PumpTrack</span>
+            </div>
+            <SidebarPinButton />
           </div>
         </SidebarHeader>
         <SidebarContent>
-          <SidebarNavigationItems /> {/* Use the inner component here */}
+          <SidebarNavigationItems />
         </SidebarContent>
         <SidebarFooter className="mt-auto p-2"> 
-          <SidebarPinButton />
+          {/* SidebarPinButton was here, now moved to SidebarHeader */}
         </SidebarFooter>
       </Sidebar>
       <SidebarInset>
