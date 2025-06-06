c2036ee82a8e56ebb74266d6cf6952c48ff51c41
Mark Hintz <mhintz1980@gmail.com>
2025-06-06 18:33:33 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <EnhancedHeader ti

--- src/components/layout/EnhancedHeader.tsx
+++ src/components/layout/EnhancedHeader.tsx
@@ -64,7 +64,7 @@ export function EnhancedHeader({
   return (
     <TooltipProvider>
       <div 
-        className="p-4 sticky top-0 z-20 bg-transparent flex items-center"
+        className="px-4 py-2 sticky top-0 z-20 bg-transparent flex items-center"
         style={{ height: 'var(--header-height-value)' }}
       >
         <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between gap-4">
