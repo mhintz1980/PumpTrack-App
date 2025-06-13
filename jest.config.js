dd81d0eea7ebd9013f291fd51cf3017ec7d8b9ae
mhintz1980 <82207035+mhintz1980@users.noreply.github.com>
2025-06-13 16:37:41 -0400

    Fix tooltip styles and update lint config

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest'
+    '^.+\\.(ts|tsx)$': ['babel-jest', { configFile: './babel-jest.config.js' }]
   }
 };
