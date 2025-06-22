8924a5e341a8123a62a1757784eaf48adcbd13b3
Mark Hintz <mhintz1980@gmail.com>
2025-06-22 04:35:15 +0000

    test: shim Response.json for NextResponse compatibility

--- jest.setup.ts
+++ jest.setup.ts
@@ -6,3 +6,12 @@ global.fetch    = fetch;
 global.Request  = fetch.Request;
 global.Response = fetch.Response;
 global.Headers  = fetch.Headers;
+
+// --- shim Response.json so NextResponse.json works under Jest ---
+if (typeof Response !== 'undefined' && typeof Response.json !== 'function') {
+  Response.json = (data, init = {}) =>
+    new Response(JSON.stringify(data), {
+      ...init,
+      headers: { 'Content-Type': 'application/json', ...(init.headers || {}) },
+    });
+}
