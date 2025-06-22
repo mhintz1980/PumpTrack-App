8257087928bc7065d062fa2d0678cc91cabbd86d
Mark Hintz <mhintz1980@gmail.com>
2025-06-22 07:35:59 +0000

    chore(test): inline babel-jest config; remove root babel.config.js to keep Turbopack happy

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   testEnvironment: 'node',
   setupFiles: ['<rootDir>/jest.setup.ts'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
   transform: {
-    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest',   // use Babel for TS/JS
+    '^.+\\.(ts|tsx|js|jsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          ['@babel/preset-env', { targets: { node: 'current' } }],
+          '@babel/preset-typescript',
+        ],
+      },
+    ],
   },
 };
